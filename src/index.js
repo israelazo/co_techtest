611878a890967585faebad439ff8a400aca40686
Israel Lazo <israel.lazo@peanuthuib.cl>
2019-10-21 17:01:56 -0300

    coverage test command

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const productPrinter = function (product) {
     console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
 };
 
-for (let i = 1; i <= 30; i += 1) {
+for (let i = 1; i <= 1; i += 1) {
     console.log(`Day ${i}`);
     console.log('name, sellIn, price');
     carInsurance.updatePrice().forEach(productPrinter);
--- src/tests/classes/products/CarInsurance.test.js
+++ src/tests/classes/products/CarInsurance.test.js
@@ -1,12 +1,21 @@
 const Product = require('../../../classes/Product').Product;
 const CarInsurance = require('../../../classes/products/CarInsurance').CarInsurance;
 
-test('New CarInsurance is created', () => {
-    const products = [
-        new Product('Medium Coverage', 10, 20),
-        new Product('Full Coverage', 2, 0),
-    ];
+const products = [
+    new Product('Medium Coverage', 10, 20),
+    new Product('Full Coverage', 2, 0),
+];
 
+test('New CarInsurance is created', () => {
     const carInsurance = new CarInsurance(products);
     expect(carInsurance.products.length).toBe(2);
+});
+
+test('CarInsurance prices are updated correctly', () => {
+    const carInsurance = new CarInsurance(products);
+
+    carInsurance.updatePrice();
+
+    expect(carInsurance.products[0].sellIn).toBe(9);
+    expect(carInsurance.products[0].price).toBe(19);
 });
